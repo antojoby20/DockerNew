943a18f13753a0208330b4e61baf58788ac3bf62
Joseph <2085786@cognizant.com>
2022-03-10 12:06:49 +0530

    new project

---
+++ Dockerfile/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dockerfile.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public string Get()
+        {
+            return ("this is a docker file");
+        }
+    }
+}
